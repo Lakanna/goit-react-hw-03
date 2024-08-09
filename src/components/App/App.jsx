233614b0afec6024efc40c37bbaf2ab98ffeb17f
Lakanna <annalakus@ukr.net>
2024-08-09 23:54:17 +0100

    finish

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,55 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import css from './App.module.css'
-import ContactList from '../ContactList/ContactList'
-import ContactForm from '../ContactForm/ContactForm'
-import SearchBox from '../SearchBox/SearchBox'
-
-
-
-
+import css from "./App.module.css";
+import ContactList from "../ContactList/ContactList";
+import ContactForm from "../ContactForm/ContactForm";
+import SearchBox from "../SearchBox/SearchBox";
 
 function App() {
- 
-  const [contactList, setContactList] = useState([
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]      
-  )
+  const [contactList, setContactList] = useState(() => {
+    return JSON.parse(localStorage.getItem("savedContact")) || [];
+  });
 
-  const [filterName, setFilterName] = useState('')
-  
-  const valueByFilter = contactList.filter((contact)=>contact.name.toLowerCase().includes(filterName.toLowerCase()))
+  const [filterName, setFilterName] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("savedContact", JSON.stringify(contactList));
+  }, [contactList]);
+
+  const valueByFilter = contactList.filter((contact) =>
+    contact.name.toLowerCase().includes(filterName.toLowerCase())
+  );
 
   function addContact(newContact) {
     setContactList((prevStat) => {
       console.log(prevStat);
       console.log(newContact);
-      return [...prevStat, newContact]
-    })
+      return [...prevStat, newContact];
+    });
   }
 
   function deleteContact(contactId) {
     setContactList((prevState) => {
-        return prevState.filter(({id})=> id!==contactId)
-      })
+      return prevState.filter(({ id }) => id !== contactId);
+    });
   }
-  
 
-return (<div>
-          <h1 className={css.title}>Phonebook</h1>
-          <ContactForm onAddContact={addContact}/>
-          <SearchBox value={ filterName} onFilter={setFilterName} /> 
-          <ContactList contactList={valueByFilter} onDelete={deleteContact} />
-        </div>)
- 
+  return (
+    <div>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm onAddContact={addContact} />
+      <SearchBox value={filterName} onFilter={setFilterName} />
+      <ContactList contactList={valueByFilter} onDelete={deleteContact} />
+    </div>
+  );
 }
 
-
-
-
-
-export default App
+export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,43 +1,57 @@
 import { useId } from "react";
 import { Formik, Form, Field } from "formik";
 import { ErrorMessage } from "formik";
-import { nanoid } from 'nanoid'
-import * as Yup from 'yup';
-import css from './ContactForm.module.css'
+import { nanoid } from "nanoid";
+import * as Yup from "yup";
+import css from "./ContactForm.module.css";
 
-export default function ContactForm({onAddContact}) {
+export default function ContactForm({ onAddContact }) {
+  const fieldNameId = useId();
+  const fieldNumberId = useId();
 
-    const fieldNameId = useId();
-    const fieldNumberId = useId();
+  const FeedbackSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "To short!")
+      .max(50, "To long!")
+      .required("Required"),
+    number: Yup.string()
+      .matches(/^\d{3}-\d{2}-\d{2}$/, "Must be XXX-XX-XX")
+      .required("Required"),
+  });
 
-    const FeedbackSchema = Yup.object().shape({
-        name: Yup.string().min(3, "To short!").max(50, "To long!").required("Required"),
-        number: Yup.number().min(6, "To short!").required("Required"),
-    });
+  function handleSubmit(values, actions) {
+    console.log(values);
+    const newContact = { ...values, id: nanoid() };
+    console.log(newContact);
+    onAddContact(newContact);
+    actions.resetForm();
+  }
 
-    function handleSubmit(values, actions) {
-        console.log(values);
-        const newContact = { ...values, id: nanoid() };
-        console.log(newContact);
-        onAddContact(newContact);
-        actions.resetForm()
- }
+  return (
+    <Formik
+      initialValues={{ name: "", number: "" }}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      <Form className={css.formAddContact}>
+        <label htmlFor={fieldNameId}>Name</label>
+        <Field type="text" name="name" id={fieldNameId}></Field>
+        <ErrorMessage className={css.errorText} name="name" component="p" />
 
-    return (
-        <Formik initialValues={{ name: "", number:"" }} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
-            <Form className={css.formAddContact}>
-                <label htmlFor={fieldNameId}>Name</label>
-                <Field type="text" name="name" id={fieldNameId}></Field>
-                <ErrorMessage className={css.errorText} name="name" component='p'/>
+        <label htmlFor={fieldNumberId}>Number</label>
+        <Field
+          type=""
+          name="number"
+          id={fieldNumberId}
+          placeholder="XXX-XX-XX"
+          autocomplit="off"
+        ></Field>
+        <ErrorMessage className={css.errorText} name="number" component="p" />
 
-                <label htmlFor={fieldNumberId}>Number</label>
-                <Field type="" name="number" id={fieldNumberId}></Field>
-                <ErrorMessage className={css.errorText} name="number" component='p'/>
-
-                <button type="submit">Add contact</button>
-            </Form>
-        </Formik>
-    )
-    
-
+        <button className={css.formaBtn} type="submit">
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
 }
